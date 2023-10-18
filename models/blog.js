c66c76781339ac99f6f885acfffc78a8982072e9
Andrew Osei-Owusu <nanaosei2089@gmail.com>
2023-10-18 06:37:33 +0000

    add user and blog models

---
+++ models/blog.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const BlogSchema = new Schema({
+  title: { type: String, required: true, unique: true },
+  description: { type: String, required: true },
+  author: { type: String, required: true },
+  state: { type: String, default: 'draft', enum: ['draft', 'published'] },
+  read_count: { type: Number },
+  reading_time: { type: Number },
+  tags: { type: [] },
+  body: { type: String, required: true },
+  user_id: { type: ObjectId, required: true },
+  created_at: { type: Date, default: new Date() },
+});
+
+// before save
+BlogSchema.pre('save', async function (next) {
+  const blog = this;
+  const time = 5000;
+
+  this.reading_time = time;
+  next();
+});
+
+const BlogModel = mongoose.model('blogs', BlogSchema);
+
+module.exports = BlogModel;
