c34b8555de11ac0583d07aa6afa39611fb892bcc
Andrew Osei-Owusu <nanaosei2089@gmail.com>
2023-10-18 06:38:41 +0000

    add user and blog models (#2)

---
+++ models/user.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  first_name: { type: String, required: true },
+  last_name: { type: String, required: true },
+  email: { type: String, unique: true, required: true },
+  password: { type: String, required: true },
+  created_at: { type: Date, default: new Date() },
+});
+
+// before save
+UserSchema.pre('save', async function (next) {
+  const user = this;
+  const hash = await bcrypt.hash(this.password, 10);
+
+  this.password = hash;
+  next();
+});
+
+UserSchema.methods.isValidPassword = async function (password) {
+  const user = this;
+  const compare = await bcrypt.compare(password, user.password);
+
+  return compare;
+};
+
+const UserModel = mongoose.model('users', UserSchema);
+
+module.exports = UserModel;
