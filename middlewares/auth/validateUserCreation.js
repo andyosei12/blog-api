1a6ed66861fd2acf43354de671885dc010e8fa2f
Andrew Osei-Owusu <nanaosei2089@gmail.com>
2023-10-18 08:01:11 +0000

    feat/add user authentication (#3)

---
+++ middlewares/auth/validateUserCreation.js
@@ -0,0 +1,22 @@
+const Joi = require('joi');
+
+const validateUserCreation = async (req, res, next) => {
+  console.log(req.body);
+  const schema = Joi.object({
+    first_name: Joi.string().required(),
+    last_name: Joi.string().required(),
+    email: Joi.string().email().required(),
+    password: Joi.string().min(8).required(),
+  });
+
+  try {
+    await schema.validateAsync(req.body, { abortEarly: true });
+    next();
+  } catch (err) {
+    return res.status(422).json({
+      message: err.message,
+    });
+  }
+};
+
+module.exports = validateUserCreation;
