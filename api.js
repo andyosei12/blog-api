d3b4cc3043d9b7336ffc045f2d7b93d5b37fd26a
Andrew Osei-Owusu <nanaosei2089@gmail.com>
2023-10-19 12:16:22 +0000

    feat/fetch user specific blogs

--- api.js
+++ api.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
-const userRouter = require('./routes/auth');
+const userRouter = require('./routes/user');
 const blogRouter = require('./routes/blog');
 
 const app = express();
@@ -10,5 +10,13 @@ app.use(express.json());
 
 app.use('/api/v1/user', userRouter);
 app.use('/api/v1/blogs', blogRouter);
+
+// globah error handler
+app.use((err, req, res, next) => {
+  res.status(500).json({
+    data: null,
+    error: 'Server Error',
+  });
+});
 
 module.exports = app;
