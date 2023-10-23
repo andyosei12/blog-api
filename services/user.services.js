585bdab3d05392299452f873023bfae22449d470
Andrew Osei-Owusu <nanaosei2089@gmail.com>
2023-10-23 11:10:13 +0000

    add auth views (#10)

---
+++ services/user.services.js
@@ -0,0 +1,90 @@
+const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
+const User = require('../models/user');
+
+dotenv.config();
+
+const registerUser = async ({ first_name, last_name, email, password }) => {
+  //   Check if email exists
+  try {
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return {
+        code: 409,
+        error: true,
+        message: 'User already exists',
+      };
+    }
+
+    const user = await User.create({
+      first_name,
+      last_name,
+      email,
+      password,
+    });
+
+    const token = jwt.sign(
+      { email: user.email, firstName: user.first_name, id: user._id },
+      process.env.JWT_SECRET,
+      { expiresIn: '1h' }
+    );
+    return {
+      code: 201,
+      data: {
+        token,
+        user,
+      },
+      message: 'User added successfully',
+    };
+  } catch (err) {
+    return {
+      code: 500,
+      error: true,
+      message: err.message,
+    };
+  }
+};
+
+const loginUser = async ({ email, password }) => {
+  //   Check if user exists
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return {
+        error: true,
+        message: 'User does not exist. Try signing up',
+        code: 404,
+      };
+    }
+
+    const validPassword = await user.isValidPassword(password);
+    if (!validPassword) {
+      return {
+        error: true,
+        code: 401,
+        message: 'Email or password is not correct',
+      };
+    }
+    const token = jwt.sign(
+      { email: user.email, firstName: user.first_name, id: user.id },
+      process.env.JWT_SECRET,
+      { expiresIn: '1h' }
+    );
+    return {
+      code: 200,
+      data: {
+        token,
+        user,
+      },
+      message: 'Login successfully',
+    };
+  } catch (error) {
+    return {
+      code: 500,
+      error: true,
+      message: error.message,
+    };
+  }
+};
+
+module.exports = { registerUser, loginUser };
--- views/views.router.js
+++ views/views.router.js
@@ -1,12 +1,59 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
 const { getBlogs } = require('../services/blog.services');
+const { registerUser, loginUser } = require('../services/user.services');
 
 const router = express.Router();
+router.use(cookieParser());
 
 router.get('/', async (req, res) => {
   const blogs = await getBlogs();
-  console.log(blogs);
-  res.render('index', { pageTitle: 'Curated', blogs: blogs.items });
+  let user = null;
+  if (req.cookies.jwt) {
+    const decodedValue = jwt.verify(req.cookies.jwt, process.env.JWT_SECRET);
+    user = decodedValue;
+  }
+  res.render('index', { pageTitle: 'Curated', blogs: blogs.items, user });
+});
+
+router.get('/user/login', async (req, res) => {
+  res.render('login', { pageTitle: 'Login', error: null });
+});
+
+router.post('/user/login', async (req, res) => {
+  const response = await loginUser(req.body);
+  if (response.error) {
+    return res.render('login', {
+      error: response.message,
+      pageTitle: 'Curated | Login',
+    });
+  } else {
+    res.cookie('jwt', response.data.token);
+    res.redirect('/');
+  }
+});
+
+router.get('/user/signup', async (req, res) => {
+  res.render('signup', { pageTitle: 'Signup', error: null });
+});
+
+router.post('/user/signup', async (req, res) => {
+  const response = await registerUser(req.body);
+  if (response.error) {
+    return res.render('signup', {
+      error: response.message,
+      pageTitle: 'Curated | Create an account',
+    });
+  } else {
+    res.cookie('jwt', response.data.token);
+    res.redirect('/');
+  }
+});
+
+router.get('/user/logout', (req, res) => {
+  res.clearCookie('jwt');
+  res.redirect('/');
 });
 
 module.exports = router;
